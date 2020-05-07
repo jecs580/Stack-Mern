8432fb5ce2180fc0a923265fed7a1d9edd08f169
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-05-06 20:06:24 -0400

    Creacion navegacion funcional con estilos de bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import './App.css';
-import Navigation from './components/navigation';
-import CreateNote from './components/createNote';
-import CreateUser from './components/createUser';
-import NotesList from './components/notesList';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+
+
+import Navigation from "./components/navigation";
+import CreateNote from "./components/createNote";
+import CreateUser from "./components/createUser";
+import NotesList from "./components/notesList";
 function App() {
   return (
-  <Router>
-    <Navigation/>
-    <Route path="/" component = {NotesList}/>
-    <Route path="/edit/:id" component = {CreateNote}/>
-    <Route path="/create" component = {CreateNote}/>
-    <Route path="/user" component = {CreateUser}/>
-  </Router>
+    <Router>
+      <Navigation/>
+      <Route exact path="/" component={NotesList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
